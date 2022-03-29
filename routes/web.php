0deb61bf08606b4b83ef22e09a971a27c3d29e4a
gustiarvilana <guztiar.profile@gmail.com>
2022-03-29 11:38:56 +0700

    route index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('clear-cache',function(){
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    Artisan::call('optimize:clear');
+    return '<h1 style="margin-top:100"> <center>Uraa!</center></hi>';
 });
